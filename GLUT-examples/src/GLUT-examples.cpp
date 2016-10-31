9d21427134ee5efcc39fc6a21aa3f61614d0a9a6
daniele <daniele.email0@gmail.com>
2016-10-31 11:01:34 +0100

    GLUT sandbox for homer emulator

---
+++ GLUT-examples/src/GLUT-examples.cpp
@@ -0,0 +1,15 @@
+//============================================================================
+// Name        : GLUT-examples.cpp
+// Author      : Daniele Colombo
+// Version     :
+// Copyright   : GPL 2
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "GLUT-examples" << endl; // prints GLUT-examples
+	return 0;
+}
