65a6209dc2629331c9fccb30753544a8a46523dd
daniele <daniele.email0@gmail.com>
2016-11-06 16:24:48 +0100

    Texture without glfw

--- GLUT-examples/src/GLUT-examples.cpp
+++ GLUT-examples/src/GLUT-examples.cpp
@@ -8,72 +8,56 @@
 
 //include header file for glfw library so that we can use OpenGL
 
-#include <GL/glfw.h>
-#include <stdlib.h>  //needed for exit function
+#include <GL/freeglut.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
 #include <iostream>
 
 using namespace std;
-
-//Initializes 3D rendering
-void
-initializeRendering ()
-
-{
-  glfwInit ();
-  //Makes 3D drawing work when something is in front of something else
-  glEnable (GL_DEPTH_TEST);
-  //glEnable( GL_TEXTURE_2D );
-}
-
+char bitmap[800][129][3];
 //Called when a key is pressed
-void GLFWCALL handleKeypress(int key,int press)//The key that was pressed
-  {
-
-    switch (key)
-      {
-	case GLFW_KEY_ESC: //Escape key
-	exit(0);//Exit the program
-      }
-  }
-
-GLuint
-LoadTexture (const char* TextureName)
-
+void
+keypress (unsigned char key, int x, int y)
 {
 
-  GLuint Texture;  //variable for texture
-  glGenTextures (1, &Texture); //allocate the memory for texture
-  glBindTexture (GL_TEXTURE_2D, Texture); //Binding the texture
-  if (glfwLoadTexture2D (TextureName, GLFW_BUILD_MIPMAPS_BIT))
+  switch (key)
     {
-      glTexParameteri (GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
-      glTexParameteri (GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
-      return Texture;
+    case 'q': //Escape key
+      glutLeaveMainLoop ();
     }
-  else
-    return -1;
-
 }
 
 void
 display ()
 
 {
-  glClearColor (0.0f, 0.0f, 0.0f, 0.0f); //clear background screen to black
-  //Clear information from last draw
-  glClear (GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-  glMatrixMode (GL_MODELVIEW); //Switch to the drawing perspective
-  glLoadIdentity (); //Reset the drawing perspective
-  glTranslatef (0.0f, 0.0f, 0.0f); //Translate whole scene to -ve z-axis by -35 unit
-  GLuint text2D;
-  text2D = LoadTexture (
-      "/wks/workspace/sandbox/GLUT-examples/resource/fpanel24.tga"); //loading image for texture
+  GLuint texture;
+  FILE* fp = fopen (
+      "/wks/workspace/sandbox/GLUT-examples/resource/fpanel800x129x24b.bmp",
+      "r");
+  fread (bitmap, 1, 54, fp);
+  int xy = fread (bitmap, 3 * sizeof(char), 800 * 129, fp);
+  if (xy != 800 * 129)
+    {
+      cerr << "Wrong size " << xy;
+      exit (1);
+    }
+  fclose (fp);
+  glGenTextures (1, &texture);
+  glBindTexture (GL_TEXTURE_2D, texture);
+  glTexParameteri (GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+  glTexParameteri (GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+  glTexImage2D (GL_TEXTURE_2D, 0, GL_RGB, 800, 129, 0, GL_RGB, GL_UNSIGNED_BYTE,
+		bitmap);
 
+  glClear (GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
   glEnable (GL_TEXTURE_2D);
-  glBindTexture (GL_TEXTURE_2D, text2D);
-  glPushMatrix ();
+  glTexEnvf (GL_TEXTURE_ENV, GL_TEXTURE_ENV_MODE, GL_REPLACE);
+  glTranslatef (0.0, 0.0, 0.0);
+  glColor3f (0.0, 0.0, 0.0);
+
   glBegin (GL_POLYGON);
-  glNormal3f (0.0f, 0.0f, 1.0f);
 
   GLfloat left = -1.0f;
   GLfloat right = 1.0f;
@@ -90,9 +74,13 @@ display ()
   glVertex3f (left, up, 0.0f); // up-sx
 
   glEnd ();
-  glPopMatrix ();
+  glFlush ();
   glDisable (GL_TEXTURE_2D); //Disable the texture
-  glfwSwapBuffers ();
+
+}
+void
+reshape (int w, int h)
+{
 
 }
 
@@ -102,41 +90,23 @@ main ()
 {
 
   // int     width, height;
+  char *myargv[1];
+  int myargc;
+  myargv[0] = strdup ("GLUT examples");
 
-  bool running = true;
-
-  initializeRendering ();
-  glfwOpenWindowHint (GLFW_WINDOW_NO_RESIZE, GL_TRUE);
-  if (!glfwOpenWindow (900, // width of window
-      141, //height of window
-      0,  //redbits
-      0,  //greenbits
-      0,  //bluebits
-      0,  //alphabits
-      0,  //depthbits
-      0, //stencilbits
-      GLFW_WINDOW) //mode
-      )//return false if window is not created
-
-    {
-
-      glfwTerminate (); //terminating glfw window
-
-      return 0;
-
-    }
-
-  glfwSetWindowTitle ("codeincodeblock.blogspot.com - Texture");
-  glfwSetKeyCallback (handleKeypress); //callback function to handle keypress
-  while (running) // infinite loop to draw object again and again
-
-    {              // because once object is draw then window is terminated
-
-      display ();
-      running = glfwGetWindowParam (GLFW_OPENED); //when glfw window is opened then it return true
-      //if closed then return false
-
-    }
+  glutInit (&myargc, &myargv[0]);
+  glutInitDisplayMode (GLUT_SINGLE | GLUT_RGB);
+  glutInitWindowSize (800, 129);
+  glutInitWindowPosition (100, 100);
+  glutCreateWindow ("GLUT");
+  glShadeModel (GL_FLAT);
+  glutSetOption (GLUT_ACTION_ON_WINDOW_CLOSE, GLUT_ACTION_GLUTMAINLOOP_RETURNS);
+  glEnable (GL_DEPTH_TEST);
+  glutReshapeFunc (reshape);
+  glutSetKeyRepeat (GLUT_KEY_REPEAT_OFF);
+  glutKeyboardFunc (keypress);
+  glutDisplayFunc (display);
+  glutMainLoop ();
 
   return 0;
 
