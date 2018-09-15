aa059e76c0e3b400e812f33cf41e65cc555f26e1
daniele <dany4any@gmail.com>
2018-09-15 08:48:33 +0200

    test

--- HelloWorld/src/HelloWorld.cpp
+++ HelloWorld/src/HelloWorld.cpp
@@ -8,23 +8,25 @@
 
 #include <iostream>
 #include <set>
+#include <stdio.h>
 using namespace std;
 
 int main() {
 
+  int a[5] = { 0, 1, 2, 3, 4 };
+  printf("%d\n", a);
+  return (0);
   std::set<int> s;
   s.insert(1);
   s.insert(2);
   s.insert(3);
 
-  for(std::set<int>::iterator it = s.begin(); it != s.end(); ++it) {
-    if(*it == 2) s.erase(it);
+  for (std::set<int>::iterator it = s.begin(); it != s.end(); ++it) {
+    if (*it == 2)
+      s.erase(it);
   }
 
-cout << "HelloComment" << endl; // prints HelloComment
+  cout << "HelloComment" << endl;  // prints HelloComment
 
-
-
-
-	return 0;
+  return 0;
 }
